66153fca750321cfb2d960d084a7a9d1e6a1fe37
Andrew Omori <andrewomori7@gmail.com>
2025-06-26 12:57:31 +0300

    Completes Document Structure Continued lab

---
+++ script.js
@@ -0,0 +1,6 @@
+const plantContainer = document.getElementById('plant-container');
+const searchInput = document.getElementById('searchInput');
+const typeFilter = document.getElementById('typeFilter');
+const clearFiltersBtn = document.getElementById('clearFiltersBtn');
+
+const API_URL = 'http://localhost:3000/plants';
